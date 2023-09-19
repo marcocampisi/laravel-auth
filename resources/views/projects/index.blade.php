eb7006f04eec305ee1593e9ea31f4368c8c8c24a
marcocampisi <marco.campisi@outlook.it>
2023-09-19 17:47:40 +0200

    Add link to navabr

--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -1,25 +1,34 @@
-@extends('layouts.app')
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Elenco dei Progetti') }}
+        </h2>
+    </x-slot>
 
-@section('content')
-<div class="container mx-auto p-6">
-    <h1 class="text-3xl font-semibold mb-6">Elenco dei Progetti</h1>
-
-    @if(count($projects) > 0)
-        <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-            @foreach($projects as $project)
-                <li class="bg-white rounded-lg shadow-md overflow-hidden">
-                    <div class="p-4">
-                        <h2 class="text-xl font-semibold mb-2">{{ $project->title }}</h2>
-                        <p class="text-gray-600">{{ $project->description }}</p>
-                    </div>
-                    <div class="bg-gray-100 p-4 border-t border-gray-200">
-                        <a href="{{ route('projects.show', ['project' => $project]) }}" class="text-blue-500 hover:underline">Dettagli</a>
-                    </div>
-                </li>
-            @endforeach
-        </ul>
-    @else
-        <p class="text-gray-600">Nessun progetto disponibile al momento.</p>
-    @endif
-</div>
-@endsection
+    <!-- Contenuto della pagina Projects -->
+    <div class="container mx-auto p-6">
+        @if(count($projects) > 0)
+            <ul class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+                @foreach($projects as $project)
+                    <li class="bg-white rounded-lg shadow-md overflow-hidden">
+                        <div class="p-4">
+                            <h2 class="text-xl font-semibold mb-2">{{ $project->title }}</h2>
+                            <p class="text-gray-600">{{ $project->description }}</p>
+                        </div>
+                        <div class="bg-gray-100 p-4 border-t border-gray-200">
+                            <a href="{{ route('projects.show', ['project' => $project]) }}" class="text-blue-500 hover:underline">Dettagli</a>
+                        </div>
+                    </li>
+                @endforeach
+            </ul>
+        @else
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900">
+                    {{ __("Nessun progetto disponibile.") }}
+                </div>
+            </div>
+        </div>
+        @endif
+    </div>
+</x-app-layout>
